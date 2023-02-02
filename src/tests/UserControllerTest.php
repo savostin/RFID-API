ce9b099aa876962a1a4d68ec4565cc881fc1fe6b
Serge Savostin <serge@savostin.com>
2023-02-02 12:27:06 +0000

    [+] More tests

--- src/tests/UserControllerTest.php
+++ src/tests/UserControllerTest.php
@@ -16,15 +16,29 @@ final class UserControllerTest extends AbstractControllerTest
 	}
 
 	public function testFindByRfid()
-	{
-		$request = (new RequestFactory)->createRequest('GET', '/user/rfid/142594708f3a5a3ac2980914a0fc954f');
-		$response = (new ResponseFactory)->createResponse();
-
-		$response = $this->controller->findByRfid($request, $response, ['rfid' => '142594708f3a5a3ac2980914a0fc954f']);
-
-		$this->assertEquals(200, $response->getStatusCode());
-		$this->assertJson((string)$response->getBody());
+	{ {
+			// not found
+			$request = (new RequestFactory)->createRequest('GET', '/user/rfid/notfound');
+			$response = (new ResponseFactory)->createResponse();
+			$response = $this->controller->findByRfid($request, $response, ['rfid' => 'notfound']);
+			$this->assertEquals(404, $response->getStatusCode());
+			$body = (string) $response->getBody();
+			$this->assertJson($body);
+			$json = json_decode($body);
+			$this->assertEquals($json->error, 404);
+		} {
+			// correct value
+			$request = (new RequestFactory)->createRequest('GET', '/user/rfid/142594708f3a5a3ac2980914a0fc954f');
+			$response = (new ResponseFactory)->createResponse();
+			$response = $this->controller->findByRfid($request, $response, ['rfid' => '142594708f3a5a3ac2980914a0fc954f']);
+			$this->assertEquals(200, $response->getStatusCode());
+			$body = (string) $response->getBody();
+			$this->assertJson($body);
+			$json = json_decode($body);
+			$this->assertEquals($json->id, 1);
+			$this->assertEquals($json->full_name, 'Julius Caesar');
+			asort($json->departments);
+			$this->assertEquals(['development', 'director'], $json->departments);
+		}
 	}
-
-
 }
